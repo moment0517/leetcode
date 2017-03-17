5c103cf94f29028bda1d73815ebf2650825ecba4
moment0517 <lssc4205@126.com>
2017-03-17 23:22:34 +0800

    Create LengthOfLongestString.scala

---
+++ LengthOfLongestString.scala
@@ -0,0 +1,23 @@
+/** Problem url: https://leetcode.com/problems/longest-substring-without-repeating-characters/#/description
+  * Created by leijun on 17/03/2017.
+  */
+
+import scala.collection.mutable.HashMap
+
+object LengthOfLongestString {
+
+  def lengthOfLongestString(s: String): Int = {
+    val map = HashMap.empty[Char, Int]
+    var i = 0
+    var posTag = 0
+    var ans = 0
+    for (c <- s) {
+      i += 1
+      if (map.contains(c))
+        posTag = map(c)
+      map(c) = i
+      ans = ans.max(i - posTag)
+    }
+    ans
+  }
+}
