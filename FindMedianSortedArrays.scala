bb94bfa794142059db537bc195775a28345e7eb1
moment0517 <lssc4205@126.com>
2017-03-18 15:43:52 +0800

    Create FindMedianSortedArrays.scala

---
+++ FindMedianSortedArrays.scala
@@ -0,0 +1,64 @@
+/** Problem url: https://leetcode.com/problems/median-of-two-sorted-arrays/#/description
+  * Example:
+  * nums1 = [1, 3]
+  * nums2 = [2]
+  *
+  * The median is 2.0
+  * Created by leijun on 18/03/2017.
+  */
+class FindMedianSortedArrays {
+  def findMedianSortedArrays(nums1: Array[Int], nums2: Array[Int]): Double = {
+    val (a, b) =
+      if (nums1.length < nums2.length)
+        (nums1, nums2)
+      else
+        (nums2, nums1)
+    val (m, n) = (a.length, b.length)
+
+    var (i_min, i_max) = (0, m)
+    val half_len = (m + n + 1) / 2
+    var med = 0.0
+
+    while (i_min <= i_max) {
+      var i = (i_min + i_max) / 2
+      var j = half_len - i
+
+      if (i == 0 || j == n || a(i - 1) <= b(j)) {
+        if (j == 0 || i == m || b(j - 1) <= a(i)) {
+          i_min = i_max + 1
+          var max_left, min_right = 0
+          if (i == 0) {
+            max_left = b(j - 1)
+          }
+          else if (j == 0) {
+            max_left = a(i - 1)
+          }
+          else {
+            max_left = a(i - 1).max(b(j - 1))
+          }
+
+          if (i == m) {
+            min_right = b(j)
+          }
+          else if (j == n) {
+            min_right = a(i)
+          }
+          else {
+            min_right = a(i).min(b(j))
+          }
+
+          if ((m + n) % 2 == 1)
+            med = max_left
+          else
+            med = (max_left + min_right).toFloat / 2.0
+        }
+        else
+          i_max += 1
+      }
+      else
+        i_max -= 1
+    }
+    med
+  }
+
+}
